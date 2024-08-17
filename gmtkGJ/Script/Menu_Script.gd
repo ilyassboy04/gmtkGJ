220cc676189cb725f925080b2d010c843b93b2d0
ilyassboy04 <elsah1344@oslomet.no>
2024-08-17 12:47:20 +0200

    options scene added w/bug btn bug and filepath mismatch

--- gmtkGJ/Script/Menu_Script.gd
+++ gmtkGJ/Script/Menu_Script.gd
@@ -3,14 +3,26 @@ extends Control
 
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
 @onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
+@onready var options_button = $MarginContainer/HBoxContainer/VBoxContainer/Options_Button
+
+
+
 @onready var start_level = preload("res://testscene_for_git.tscn") as PackedScene
 
+
 func _ready():
-	start_button.button_down.connect(on_start_pressed)
-	exit_button.button_down.connect(on_exit_pressed)
+	handle_donnecting_signals()
 	
 func on_exit_pressed() -> void:
 	get_tree().quit()
 	
 func on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
+
+func on_options_pressed() -> void:
+	print("load options menu")
+	
+func handle_donnecting_signals() -> void:
+	start_button.button_down.connect(on_start_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+	options_button.button_down.connect(on_options_pressed)
