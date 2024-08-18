22282bc99f4fd078dd2f753373355db6485d17ee
ilyassboy04 <elsah1344@oslomet.no>
2024-08-18 14:43:37 +0200

    issue fix, adding keybinds hotkeys

---
+++ gmtkGJ/Scenes/Options Menu/hot_key_rebind_button.gd
@@ -0,0 +1,9 @@
+class_name HotKeyRebindButton
+extends Control
+
+@onready var label = $HBoxContainer/Label as Label
+@onready var button = $HBoxContainer/Button as Button
+
+@export var action_name : String = ""
+
+
