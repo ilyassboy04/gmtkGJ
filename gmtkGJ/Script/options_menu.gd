1b07e5cf0fb2b98d621bbd40900e78964e7fd68f
ilyassboy04 <elsah1344@oslomet.no>
2024-08-18 12:47:26 +0200

    endeliggggg

--- gmtkGJ/Script/options_menu.gd
+++ gmtkGJ/Script/options_menu.gd
@@ -1,16 +1,17 @@
 class_name OptionsMenu
 extends Control
 
-@onready var exit_button = $"../MarginContainer/HBoxContainer/VBoxContainer/Exit_Button" as Button
-
-
+@onready var exit_options_button = $MarginContainer/VBoxContainer/Exit_Options_Button as Button
 
 signal exit_options_menu
 
+
 func _ready():
-	exit_button.button_down.connect(on_exit_pressed)
+	
+	exit_options_button.button_down.connect(on_exit_pressed)
 	set_process(false)
 	
+
 func on_exit_pressed() -> void:
 	exit_options_menu.emit()
 	set_process(false)
