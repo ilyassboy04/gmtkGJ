323ee053da673c3845f252b2c046ecdef9fbdc69
ilyassboy04 <elsah1344@oslomet.no>
2024-08-18 16:59:47 +0200

    tømme røret

--- gmtkGJ/Scenes/Options Menu/hot_key_rebind_button.gd
+++ gmtkGJ/Scenes/Options Menu/hot_key_rebind_button.gd
@@ -4,6 +4,9 @@ extends Control
 @onready var label = $HBoxContainer/Label as Label
 @onready var button = $HBoxContainer/Button as Button
 
-@export var action_name : String = ""
+@export var action_name : String = "move_left"
 
+func _ready():
+	set_process_unhandled_input(false)
+	
 
